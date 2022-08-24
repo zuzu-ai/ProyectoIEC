1ffc8f073eab2e1711163c76ae3a7a7adaf4cd04
kievkeivn13 <kievkevin13@gmail.com>
2022-08-23 21:35:09 -0600

    Subada Proyecto IEC y Navegador

---
+++ Proyecto IEC/Proyecto IEC/frmMDI_IEC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_IEC
+{
+	public partial class frmMDI_IEC : Form
+	{
+		public frmMDI_IEC()
+		{
+			InitializeComponent();
+		}
+
+        private void empleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                frmEmpleado form = new frmEmpleado();
+                form.MdiParent = this;
+                form.Show();
+            }
+            catch (Exception ex) { MessageBox.Show("Error: " + ex); }
+        }
+    }
+}
