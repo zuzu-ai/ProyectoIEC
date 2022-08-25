cada9de66f09d4eefab665075fcf73c044032fb3
kievkeivn13 <kievkevin13@gmail.com>
2022-08-24 21:37:27 -0600

    Importacion de archivos preliminar

---
+++ Proyecto IEC/CapaContoladorProyectoIEC/Controlador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using CapaModeloProyectoIEC;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaContoladorProyectoIEC
+{
+    public class Controlador
+    {
+        private Sentencias sn = new Sentencias();
+        public DataTable EncontrarArchivoExcelControlador(string NombreArchivo, string NombreTabla)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                table = sn.EncontrarArchivoExcel(NombreArchivo, NombreTabla);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al consultar archivo de Excel (Controlador).");
+            }
+            return table;
+        }
+        public void guardarTablaBdDetalleControlador(string tiempo,string estado,string dispositivos,string tipoResgistro)
+        {
+            sn.guardarTablaBdDetalleSentencias( tiempo, estado, dispositivos, tipoResgistro);
+        }
+        public void guardarTablaBdEncabezadoControlador(string fkempleado, string fkdispositivo, string estado)
+        {
+            sn.guardarTablaBdEncabezadoSentencias(fkempleado, fkdispositivo, estado);
+        }
+    }
+}
--- Proyecto IEC/Proyecto IEC/frmImportarArchivo.Designer.cs
+++ Proyecto IEC/Proyecto IEC/frmImportarArchivo.Designer.cs
@@ -35,6 +35,7 @@ namespace Proyecto_IEC
             this.btnExportar = new System.Windows.Forms.Button();
             this.txtNombreHoja = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnCnacelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,12 +43,13 @@ namespace Proyecto_IEC
             // 
             this.dgvVistaPrevia.AllowUserToAddRows = false;
             this.dgvVistaPrevia.AllowUserToDeleteRows = false;
+            this.dgvVistaPrevia.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvVistaPrevia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvVistaPrevia.Location = new System.Drawing.Point(12, 62);
             this.dgvVistaPrevia.Name = "dgvVistaPrevia";
             this.dgvVistaPrevia.ReadOnly = true;
             this.dgvVistaPrevia.RowHeadersWidth = 51;
-            this.dgvVistaPrevia.Size = new System.Drawing.Size(726, 311);
+            this.dgvVistaPrevia.Size = new System.Drawing.Size(709, 401);
             this.dgvVistaPrevia.TabIndex = 100;
             // 
             // btnImportar
@@ -57,7 +59,7 @@ namespace Proyecto_IEC
             this.btnImportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnImportar.FlatAppearance.BorderSize = 0;
             this.btnImportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnImportar.Location = new System.Drawing.Point(489, 8);
+            this.btnImportar.Location = new System.Drawing.Point(506, 9);
             this.btnImportar.Margin = new System.Windows.Forms.Padding(0);
             this.btnImportar.Name = "btnImportar";
             this.btnImportar.Size = new System.Drawing.Size(50, 50);
@@ -72,16 +74,17 @@ namespace Proyecto_IEC
             this.btnExportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnExportar.FlatAppearance.BorderSize = 0;
             this.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExportar.Location = new System.Drawing.Point(551, 8);
+            this.btnExportar.Location = new System.Drawing.Point(606, 9);
             this.btnExportar.Margin = new System.Windows.Forms.Padding(0);
             this.btnExportar.Name = "btnExportar";
             this.btnExportar.Size = new System.Drawing.Size(50, 50);
             this.btnExportar.TabIndex = 102;
             this.btnExportar.UseVisualStyleBackColor = false;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
             // 
             // txtNombreHoja
             // 
-            this.txtNombreHoja.Location = new System.Drawing.Point(173, 25);
+            this.txtNombreHoja.Location = new System.Drawing.Point(190, 26);
             this.txtNombreHoja.Name = "txtNombreHoja";
             this.txtNombreHoja.Size = new System.Drawing.Size(301, 20);
             this.txtNombreHoja.TabIndex = 103;
@@ -91,25 +94,43 @@ namespace Proyecto_IEC
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(45, 25);
+            this.label1.Location = new System.Drawing.Point(62, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 16);
             this.label1.TabIndex = 104;
             this.label1.Text = "Nombre de la hoja:";
             // 
+            // btnCnacelar
+            // 
+            this.btnCnacelar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCnacelar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCnacelar.BackgroundImage")));
+            this.btnCnacelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCnacelar.FlatAppearance.BorderSize = 0;
+            this.btnCnacelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCnacelar.Location = new System.Drawing.Point(556, 9);
+            this.btnCnacelar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCnacelar.Name = "btnCnacelar";
+            this.btnCnacelar.Size = new System.Drawing.Size(50, 50);
+            this.btnCnacelar.TabIndex = 102;
+            this.btnCnacelar.UseVisualStyleBackColor = false;
+            this.btnCnacelar.Click += new System.EventHandler(this.btnCnacelar_Click);
+            // 
             // frmImportarArchivo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(751, 388);
+            this.ClientSize = new System.Drawing.Size(733, 475);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtNombreHoja);
+            this.Controls.Add(this.btnCnacelar);
             this.Controls.Add(this.btnExportar);
             this.Controls.Add(this.btnImportar);
             this.Controls.Add(this.dgvVistaPrevia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmImportarArchivo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmImportarArchivo";
+            this.Text = "Importar archivo";
             ((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -123,5 +144,6 @@ namespace Proyecto_IEC
         private System.Windows.Forms.Button btnExportar;
         private System.Windows.Forms.TextBox txtNombreHoja;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCnacelar;
     }
 }
